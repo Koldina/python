f3d07e436a2e7940cef382be68bd5b9cf42fe1a9
Alex <a1eksbend@gmail.com>
2022-09-28 23:55:20 +0300

    набор факториалов

---
+++ ex015_dz7.py
@@ -0,0 +1,11 @@
+#Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
+
+number = int(input('Введите число: '))
+
+factorial = 1
+lst = [1]
+for i in range(2,number+1):
+    factorial *= i 
+    lst.append(factorial)
+print(lst)
+
