2a797f4ab61713f39ff7ba872e146140fde4228c
Alex <a1ekcbend@gmail.com>
2022-09-28 12:14:15 +0300

    предикат

--- ex007_dz2.py
+++ ex007_dz2.py
@@ -1,7 +1,13 @@
 #Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 #¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z
-x = int(input('Введите x: '))
-y = int(input('Введите y: '))
-z = int(input('Введите z: '))
-#print(not x or y or z == not x and not y and not z)
-print((not(x or y or z)) == (not x and not y and not z))
+#1вар
+#x = int(input('Введите x: '))
+#y = int(input('Введите y: '))
+#z = int(input('Введите z: '))
+
+#print((not(x or y or z)) == (not x and not y and not z))
+#2вар
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            print((x, y, z), not(x or y or z) == (not x and not y and not z))
