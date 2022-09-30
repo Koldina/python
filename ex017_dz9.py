aeb2e702f5fc80e56788fe1b5eebc76e9a9f83b2
Alex <a1ekcbend@gmail.com>
2022-09-30 14:52:20 +0300

    умножение элементов по txt

---
+++ ex017_dz9.py
@@ -0,0 +1,23 @@
+#Задайте список из N элементов, заполненных числами из промежутка [-N, N].
+# Найдите произведение элементов на указанных позициях.
+# Позиции хранятся в файле file.txt в одной строке одно число
+
+
+import random
+
+n = int(input('Введите N: '))
+lst = []#создаём список
+for i in range(1, n + 1): 
+    lst.append(random.randint(-n,n))#добавляем рандомные значения в диапазоне
+print(lst)
+
+f = open('file_dz9.txt')#открываем файл
+ind1 = int(f.read(1))#считываем позицию 1
+ind2 = int(f.read(2))#считываем позицию 2
+f.close()#закрываем файл
+print(f'Indexes from file.txt: {ind1}, {ind2}')#jоткрываем файл
+
+mult = lst[ind1]*lst[ind2]
+print(mult)
+
+
