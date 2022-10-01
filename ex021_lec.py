0d74bbfe657314b0f7f6a67d481701b4dc594245
Alex <A1ekcbend@gmail.com>
2022-10-02 00:33:10 +0300

    теория

--- ex021_lec.py
+++ ex021_lec.py
@@ -11,15 +11,54 @@
 
             #Рекурсия
 
-def fib(n):
-    if n in [1, 2]:
-        return 1
-    else:
-        return fib(n-1) + fib(n-2)
+#def fib(n):
+#    if n in [1, 2]:
+#        return 1
+#    else:
+#       return fib(n-1) + fib(n-2)
 
-list = []
-for e in range(1, 10):
-    list.append(fib(e))
-print(list) # 1 1 2 3 5 8 13 21 34
+#list = []
+#for e in range(1, 10):
+#    list.append(fib(e))
+#print(list) # 1 1 2 3 5 8 13 21 34
 
-            #Кортежи
+            #Кортежи - неизменяемый список
+
+#обращаться к конкретному индексу
+
+#a = (3 , 1, 41, 4) # картеж
+#print(a)
+#print(a[0])# 0 = 3  / -1 = 4 /  -2 = 
+
+# можно перебирать
+
+for item in a:
+    print(item)
+
+            #Словари - неупорядоченные коллекции произвольных объектов с доступом по ключу
+
+dictionary = {} # \ - если текст нужно пернести по строчкам вниз
+dictionary = \
+    {
+        'up' : 'Стрелка вверх',
+        'left' : 'Стрелка влево',
+        'down' : 'Стрелка вниз',
+        'right' : 'Стрелка влево'
+    }
+
+#замена значений в словаре
+print(dictionary['up'])
+dictionary['up'] = 'upp' #замена ключа
+print(dictionary['up'])
+
+
+print(dictionary) # {'up' : 'Стрелка вверх', 'left' : 'Стрелка влево', 'down' : 'Стрелка вниз', 'right' : 'Стрелка влево'}
+print(dictionary['left']) # Стрелка влево
+
+for k in dictionary.keys():# перебор всех ключей
+    print(k)
+for k in dictionary.values():# перебор всех значений
+
+
+                #Множества
+
