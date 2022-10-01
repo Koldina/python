e51e7326f7430f353e00832dee6ee973852f9bf1
Alex <a1ekcbend@gmail.com>
2022-10-01 12:52:15 +0300

    словарь индекс- значение

--- ex014_dz6.py
+++ ex014_dz6.py
@@ -3,6 +3,13 @@
 #{1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19} - не получается скобки поставить)
 
 
+#number = int(input('Введите число: '))
+#for i in range(1,number+1):
+#    print( i, ':', 3*i +1, ',' , end = ' ')
+
+list = {}
 number = int(input('Введите число: '))
 for i in range(1,number+1):
-    print( i, ':', 3*i +1, ',' , end = ' ')
+    result = 3 * i + 1
+    list[i] = result# словарь индекс - значение
+print(f'Полученный список: {list}')
