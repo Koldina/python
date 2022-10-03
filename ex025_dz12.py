2ce9717f441e157a3cc230d659d47bb90d15100b
Alex <a1eksbend@gmail.com>
2022-10-03 23:58:03 +0300

    заготовка дз

--- ex025_dz12.py
+++ ex025_dz12.py
@@ -13,51 +13,13 @@ print(lst)
 lstNew = []#создаём новый список либо чётный либо нечётный
 
 if (len(lst) %2 == 0):
-    nNew =len(lst) / 2 
-for i in range(1, len(lst)//2 + 1):
-    new_lst = [lst[i]*lst[len(lst)-i-1]]
-print(new_lst)
+    lstNew = len(lst) / 2 #чётный
+else:
+    lstNew = len(lst) / 2 + 1 #нечётный
+    lstNew[len(lstNew) - 1] = lst[len(lst) / 2] # число без пары в последний элемент массива(по умолчанию ноль)
 
-
-int[] pairs;//новый массив либо чётный либо нечётный
-if (numbers.Length % 2 == 0)
-{
-    pairs = new int [numbers.Length / 2];//чётный
-}
-else
-{
-    pairs = new int [numbers.Length / 2 + 1];//нечётный
-    pairs[pairs.Length - 1] = numbers[numbers.Length / 2];//число без пары в последний элемент массива(по умолчанию ноль)
-}
-
-
-for (int i = 0; i < numbers.Length / 2; i++)
-    {
-       pairs[i] = numbers[i] * numbers[numbers.Length -1 -i];
-    }
-PrintArray(pairs);
-
-
-
-void FillArrayRandomNumbers(int [] array)// рандом числа в массив
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = new Random().Next(0, 5);
-    }
-}
-
-void PrintArray(int[] array)//вывод массива на экран
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write(array[i] + " ");
-    }
-Console.WriteLine();
-}
-
-int ReadInt(string message)
-{
-    Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
-}
+  
+for i in range(1, len(lst) / 2):
+    lstNew[i] = lst[i] * lst[len(lst) - 1 - i]
+lstNew.append((lstNew[len(lstNew) - 1]) ** 2 )    
+print(lstNew)
