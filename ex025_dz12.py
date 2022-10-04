1006652a801e6219f2572f0e7b3fd0fa83495d84
Alex <a1ekcbend@gmail.com>
2022-10-04 13:03:32 +0300

    произведение пар чисел

--- ex025_dz12.py
+++ ex025_dz12.py
@@ -8,18 +8,11 @@ n = int(input('Введите N: '))
 lst = []#создаём список
 for i in range(1, n + 1): 
     lst.append(random.randint(1,10))#добавляем рандомные значения в диапазоне
-print(lst)
+print("Заданнный список рандомных чисел:", lst)
 
-lstNew = []#создаём новый список либо чётный либо нечётный
+for i in range(len(lst)):
+    if i >= len(lst)/2:
+        break 
+    print(lst[i] * lst[-1 -i], end = ' ')
 
-if (len(lst) %2 == 0):
-    lstNew = len(lst) / 2 #чётный
-else:
-    lstNew = len(lst) / 2 + 1 #нечётный
-    lstNew[len(lstNew) - 1] = lst[len(lst) / 2] # число без пары в последний элемент массива(по умолчанию ноль)
 
-  
-for i in range(1, len(lst) / 2):
-    lstNew[i] = lst[i] * lst[len(lst) - 1 - i]
-lstNew.append((lstNew[len(lstNew) - 1]) ** 2 )    
-print(lstNew)
