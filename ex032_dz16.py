bbec41426416edb0b24bbeb39ed07ff346588bf2
Alex <a1ekcbend@gmail.com>
2022-10-08 08:31:35 +0300

    рабочий вариант

--- ex032_dz16.py
+++ ex032_dz16.py
@@ -9,20 +9,20 @@
 #print(result)
 #2вар - подсчёт колисества чисел
 d = str(input('Введите число d: '))
-result = len(d) -2 #вычитаем точку и первый 0
+result = len(d) #вычитаем точку и первый 0
 print(result)
 
 #Расчёт пи через функцию
 import math
 #1 вар - округление
-pi = math.pi
-print (pi)
-print (round(pi,result))# округление до нужного значения
+#pi = math.pi
+#print (pi)
+#print (round(pi,result))# округление до нужного значения
 
 
 #2 вар - обрез - не работает
-#pi = str(math.pi)
-#print (pi)
-#print(pi[:result]) 
+pi = str(math.pi)
+print (pi)
+print(pi[:result]) 
 
 
