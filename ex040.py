c3898cd7d91df582c51ca1579e20d04537199560
Alex <a1eksbend@gmail.com>
2022-10-08 15:41:59 +0300

    работа с текстом

--- ex040.py
+++ ex040.py
@@ -1,12 +1,23 @@
 # В файле находится N натуральных чисел, записанных через пробел. 
-# Среди чиселне хватает одного, чтобы выполнить условие A[i] - 1 = A[1 -1]
+# Среди чиселне хватает одного, чтобы выполнить условие A[i] - 1 = A[i -1]
 
 
-with open('ex040_file.txt', 'r') as rd: # rd - название переменной/  при такой записи файл можно не закрывать
+with open('D:\Учёба\PYTHON\python\\file_ex040.txt', 'r') as rd: # rd - название переменной/  при такой записи файл можно не закрывать
+    lst = list(map(int,rd.read().split(' ')))# rd.read().- получаем строку split - превращаем в список из строк  \ map(int - применяем функцию int к каждому обьекту \ list - создали список массив
     #print(rd.read())
     #lst = rd.read().split(' ')
     #применяем функцию map для перевода значений в int
-    lst = list(map(int.rd.read().split(' ')))
-    print(lst)
+    
+print(lst)
 #str1 = '0 1 2 3 4 6 7 8'
 #lst = str1.split(' ')
+
+for i in range(1, len(lst)):
+    if lst[i] - 1 != lst[i -1]:
+        lst.insert(i, lst[i]-1) # Метод list insert() вставляет элемент в список по указанному индексу.
+print(lst)
+
+#восстанавливаем файл
+
+with open('D:\Учёба\PYTHON\python\\file_ex040.txt', 'w') as rd:
+    rd.write(' '.join(map(str, lst)))
