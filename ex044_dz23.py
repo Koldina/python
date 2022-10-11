6032e8cae5bad55ad8de7078e89fa2d64aeefd95
Alex <a1eksbend@gmail.com>
2022-10-11 23:59:51 +0300

    не работает ткрытие файлов

--- ex044_dz23.py
+++ ex044_dz23.py
@@ -1,43 +1,33 @@
 #RLE - сжимает данные если несколько одинаковых символов подряд то сначаала пишется количество символов потом симвл ееее - 4е
+with open('D:\Учёба\PYTHON\python\\file_ex040.txt', 'r') as txt:
+    #string = 'wwwwwwwwwwwweeeeeeeeeerrrrrrrrrttttttttttyyyyyyyyyuuuuuuuuuuu'# достать из файла
+    count = 1
+    rlecod = ' '
+    for i in range(len(txt) - 1):# -1 - индексов меньше чем кол во символов  string
+        if txt[i] == rd[i+1]:# если следующий символ совпадает то счёт +1
+            count += 1
+        else:
+            rlecod = rlecod + str(count) + txt[i]# записывается в строку и склеивается
+            count = 1# счётчик обнуляется
+    if count > 1 or (txt[len(txt) - 2] != txt[-1]):# склеиваем последние символы или символ
+        rlecod = rlecod + str(count) + txt[-1]
 
-string = 'wwwwwwwwwwwweeeeeeeeeerrrrrrrrrttttttttttyyyyyyyyyuuuuuuuuuuu'# достать из файла
-count = 1
-rlecod = ' '
-for i in range(len(string) - 1):# -1 - индексов меньше чем кол во символов
-    if string[i] == string[i+1]:# если следующий символ совпадает то счёт +1
-        count += 1
-    else:
-        rlecod = rlecod + str(count) + string[i]# записывается в строку и склеивается
-        count = 1# счётчик обнуляется
-if count > 1 or (string[len(string) - 2] != string[-1]):# склеиваем последние символы или символ
-    rlecod = rlecod + str(count) + string[-1]
+    print(f'Текст после кодировки: {rlecod}')
 
-print(f'Текст после кодировки: {rlecod}')
 
-#number = ''
-#rledecod = ''
-#for i in range(len(rlecod)):
-#    if not rlecod[i].isalpha():
-#        number += rledecod[i]
-#    else:
-#        rledecod = rledecod +rlecod[i] * int(number)
-#        number = ''
-#print(f'Текст после дешифровки: {rledecod}')
-
-def unpack(file):
-    txt = open(file).read()
-    lenth = 0# счётчик
+with open('D:\Учёба\PYTHON\python\\file_ex040.txt', 'r') as txt:
+    count1 = 0# счётчик
     wt_txt = ''# переменная для записи в файл
-    while lenth < len(txt):# пока счётчик меньше длинны файла или for i in range(len(txt) - 1):
+    while count1 < len(txt):# пока счётчик меньше длинны файла или for i in range(len(txt) - 1):
         amount = '' # число
         key = ''# буква
-        while txt(lenth).isdigit():# пока симвл является цифрой isdigit - проверка на число
-            amount += txt[lenth] 
-            lenth += 1
-        key += txt[lenth]
+        while txt(count1).isdigit():# пока симвл является цифрой isdigit - проверка на число
+            amount += txt[count1] 
+            count1 += 1
+        key += txt[count1]
         wt_txt += int(amount) * key # склеиваем символы
-        lenth += 1
-    with open('hw5.txt', 'w') as text:
+        count1 += 1
+    with open('file_ex044_unpack.txt', 'w') as text:
         text.write(f'wt_txt')# запись итоговой переменной
-unpack('hm5_3.txt')
+
 
