66a62a3a3a3b8fe095c765f99133316baaae2053
Alex <a1eksbend@gmail.com>
2022-10-15 00:35:47 +0300

    решение через list comprehension

--- ex014_dz6.py
+++ ex014_dz6.py
@@ -3,13 +3,12 @@
 #{1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19} - не получается скобки поставить)
 
 
-#number = int(input('Введите число: '))
-#for i in range(1,number+1):
-#    print( i, ':', 3*i +1, ',' , end = ' ')
-
 list = {}
 number = int(input('Введите число: '))
 for i in range(1,number+1):
     result = 3 * i + 1
     list[i] = result# словарь индекс - значение
 print(f'Полученный список: {list}')
+
+
+#print(list(map(lambda i: i = 3 * i + 1 )))
