2fa9b910c08ad24157d249193e9991b2797c9728
Alex <a1eksbend@gmail.com>
2022-10-15 00:40:17 +0300

    list comp

--- ex015_dz7.py
+++ ex015_dz7.py
@@ -1,11 +1,13 @@
-#Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
+#Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N. 
+import math
 
 number = int(input('Введите число: '))
 
-factorial = 1
-lst = [1]
-for i in range(2,number+1):
-    factorial *= i 
-    lst.append(factorial)
-print(lst)
+#factorial = 1
+#lst = [1]
+#for i in range(2,number+1):
+#    factorial *= i 
+#    lst.append(factorial)
+#print(lst)
 
+print(list([math.factorial(i) for i in range(1,number+1)]))
