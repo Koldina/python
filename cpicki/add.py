5a7b22f9725ea1a4e3e00b383958fd5a8aa58e2c
Alex <a1eksbend@gmail.com>
2022-10-22 23:25:49 +0300

    списки заготовка

--- cpicki/add.py
+++ cpicki/add.py
@@ -1,10 +1,11 @@
 #добавление: ввод всех данных (добавление в массив подмассива : фамилия,имя,отчество,мобильный,домашний)
+#Ввести варианты ввод, через запятую или что то другое или с новой строки . Пользователь разделяет выбор разделителя
 
 from turtle import heading
 
 
-data_entry = ["Фамилия", "Имя", "Отчество", "Домашний тел", "Рабочий тел"]
-def add_data_entry(data_entry):# принимает на вход массив
+#data_entry = ["Фамилия", "Имя", "Отчество", "Домашний тел", "Рабочий тел"]
+def add_person(data_entry):# принимает на вход массив
     global heading# Заголовок?
     surname = input("Фамилия")
     name = input("Имя")
@@ -12,9 +13,9 @@ def add_data_entry(data_entry):# принимает на вход массив
     home_phone = input("Домашний телефон")
     work_phone = input("Рабочий телефон")
     heading = [surname, name, patronymic, home_phone, work_phone]#создаём массив
-    data_entry = np.array([data_entry,heading])
+    data_entry.append(heading)# добавляем новый массив в существующий
     return data_entry# возвращаем массив
-add_data_entry()
-import numpy as np
-data_entry = np.array([data_entry,heading])
-print(data_entry)
+#add_person(data_entry) # Вызов метода для дебага
+#print(data_entry)
+
+
